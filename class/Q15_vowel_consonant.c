55e72472ed521e8b2de1b77369855598f5e73501
Stien-arch-cs50 <143864873+Stien-arch-cs50@users.noreply.github.com>
2024-01-12 22:40:36 +0600

    12.0124

--- class/Q15_vowel_consonant.c
+++ class/Q15_vowel_consonant.c
@@ -6,5 +6,6 @@ int main (void)
     if(let=='a'||let=='A'||let=='e'||let=='E'||let=='i'||let=='I'||let=='o'||let=='O'||let=='u'||let=='U')
     printf("Taken Alphabet is a vowel\n");
     else
+    printf("It's somethin' else ¯/_(ツ)_/¯");
     return 0 ;
 }
